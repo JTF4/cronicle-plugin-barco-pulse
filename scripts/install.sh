d44ad1bd664b71a1fe54a938eeed8766a79b79ee
David Stevens <teamjtf4@gmail.com>
2022-11-09 16:27:18 -0500

    Update Installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,6 @@ echo "Installing cronicle-plugin-barco-pulse"
 mkdir -p /opt/cronicle/plugins/cronicle-plugin-barco-pulse
 
 mv ./dist/index.js /opt/cronicle/plugins/cronicle-plugin-barco-pulse
+mv ./node_modules /opt/cronicle/plugins/cronicle-plugin-barco-pulse/node_modules
+
+echo "Plugin Path: /opt/cronicle/plugins/cronicle-plugin-barco-pulse/index.js"
